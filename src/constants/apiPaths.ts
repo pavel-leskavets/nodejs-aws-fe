7eabfbafa1b98b147938aa9a47deff1e1278305a
Pavel Leskavets <pleskavets@ibagroup.eu>
2020-11-15 22:22:39 +0300

    fix: fix urls

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://https://hx1hbf93hh.execute-api.eu-west-1.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://https://hx1hbf93hh.execute-api.eu-west-1.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://1f7232xoo0.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://hx1hbf93hh.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
